07a7d03e27c660fbd1e844db5b37e19a6ef120ec
gentlable <shingo.gt.1115@gmail.com>
2022-11-22 22:06:50 +0900

    修正

--- src/main/java/oauth/OauthController.java
+++ src/main/java/oauth/OauthController.java
@@ -12,25 +12,37 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OauthController extends WebSecurityConfigurerAdapter {
 
-	@GetMapping("/user")
-	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		return Collections.singletonMap("name", principal.getAttribute("name"));
-	}
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// @formater:off
+		// @formatter:off
 		http
 			.authorizeRequests(a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
 			.exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 			.csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
 			.logout(l -> l.logoutSuccessUrl("/").permitAll())
 			.oauth2Login();
-		// @formater:on
+		// @formatter:on
 	}
 
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "test returned";
+	}
+
+	@GetMapping("/github")
+	public String github() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.getForEntity(
+				"https://google.com", String.class).toString();
+	}
 }
